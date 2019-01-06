2d6bf3bfd1f3e7224dbb274ab34b4b2cdc486226
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-06 18:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function
+function add (a,b){
+  return a += b;
+}
+function subtract(a,b){
+  return a -=  b;
+}
 function multiply(a,b){
   return a *= b;
+}
+function divide(a,b){
+  return a/=b;
 }
 function inc(n){
   return n++;
