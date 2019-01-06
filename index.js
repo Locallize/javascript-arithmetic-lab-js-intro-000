234f2d8d629ffb9a8f826cdc1dbc9d0c6d26e096
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-06 18:08:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-preserveDecimal(n){
-  return parseFloat(n)
+function
+function multiply(a,b){
+  return a *= b;
+}
+function inc(n){
+  return n++;
+}
+function dec(n){
+  return n--;
+}
+function makeInt(n){
+  return parseInt(n);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
 }
